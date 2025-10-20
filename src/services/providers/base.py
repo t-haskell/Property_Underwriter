75898a4780539e8a4d014f45d8c1e83e125dcf7c
Thomas Michael Haskell <94083215+t-haskell@users.noreply.github.com>
2025-10-20 04:12:29 +0000

    remobing unused import

--- src/services/providers/base.py
+++ src/services/providers/base.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Optional
 
 from ...core.models import Address, PropertyData
-from ...utils.scaffolding import scaffold, scaffoldable
+from ...utils.scaffolding import scaffold
 
 class PropertyDataProvider:
     """Interface all providers implement."""
